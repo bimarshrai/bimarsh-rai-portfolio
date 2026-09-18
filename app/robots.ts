3919fac774e0b4132f192fb79aac9f47957aeafa
bimarshrai <bimarshkumar@gmail.com>
2026-09-18 15:18:26 +0530

    Add sitemap and host directives to robots metadata

--- app/robots.ts
+++ app/robots.ts
@@ -1,10 +1,14 @@
 import type { MetadataRoute } from "next";
 
+const siteUrl = "https://bimarsh-rai-portfolio.vercel.app";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
+    sitemap: `${siteUrl}/sitemap.xml`,
+    host: siteUrl,
   };
 }
